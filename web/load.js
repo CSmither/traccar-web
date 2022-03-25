e8c3c9145ac295e48407c7c8fcf136e97e7512d1
Callum Smith <callum@smither.org>
2022-03-25 13:01:26 +0000

    Add all other number categories

--- web/load.js
+++ web/load.js
@@ -175,8 +175,8 @@
 
     window.Images = ['arrow', 'default', 'animal', 'bicycle', 'boat', 'bus', 'car',
         'cat1', 'cat2', 'cat3', 'cat4', 'cat5', 'cat6', 'cat7', 'cat8', 'cat9', 'cat0',
-        'catA', 'catB', 'catC', 'catD', 'catE', 'catF', 'catG', 'catH', 'catI', 'catJ', 'catK', 'catL', 'catM',
-        'catN', 'catO', 'catP', 'catQ', 'catR', 'catS', 'catT', 'catU', 'catV', 'catW', 'catX', 'catY', 'catZ',
+        // 'catA', 'catB', 'catC', 'catD', 'catE', 'catF', 'catG', 'catH', 'catI', 'catJ', 'catK', 'catL', 'catM',
+        // 'catN', 'catO', 'catP', 'catQ', 'catR', 'catS', 'catT', 'catU', 'catV', 'catW', 'catX', 'catY', 'catZ',
         'crane', 'helicopter', 'motorcycle', 'offroad', 'person', 'pickup', 'plane', 'ship', 'tractor',
         'train', 'tram', 'trolleybus', 'truck', 'van', 'scooter'];
 
